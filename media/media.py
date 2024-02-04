66ec19d5bf2be01e88f94e7fb91102a9de15a07c
igorcduarte <igorcduarte17@hotmail.com>
2024-02-04 14:47:19 -0300

    Exercicio Media Respondido

--- media/media.py
+++ media/media.py
@@ -1,2 +1,10 @@
 # Crie um script que receba 4 valores e retorne a média dos valores.
 # media = (valor1 + valor2 + valor3 + valor4)/4
+valor1 = float(input('Insira o primeiro valor: '))
+valor2 = float(input('Insira o segundo valor: '))
+valor3 = float(input('Insira o terceiro valor: '))
+valor4 = float(input('Insira o quarto valor: '))
+
+media = (valor1 + valor2 + valor3 + valor4)/4
+
+print('A média dos valores é:', media)
