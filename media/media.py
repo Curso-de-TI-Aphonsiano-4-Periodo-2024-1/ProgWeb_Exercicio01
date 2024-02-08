33098bd92414cbb670367a135f4ca3bf0fe16300
Marvery-MT7 <marveryckmt2023@gmail.com>
2024-02-07 21:21:12 -0300

    calcula media

--- media/media.py
+++ media/media.py
@@ -1,2 +1,15 @@
 # Crie um script que receba 4 valores e retorne a média dos valores.
 # media = (valor1 + valor2 + valor3 + valor4)/4
+def calcular_media(nota1, nota2, nota3, nota4):
+    soma_das_notas =nota1 + nota2 + nota3 + nota4
+    qtd_de_notas = 4
+    media_das_notas = soma_das_notas / qtd_de_notas
+    return media_das_notas
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+nota4 = float(input("Digite a quarta nota: "))
+
+media = calcular_media(nota1, nota2, nota3, nota4)
+print("A média das notas é: ", media)
