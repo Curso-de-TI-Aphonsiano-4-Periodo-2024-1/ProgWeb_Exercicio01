9c4a52fe6a0556cd47603896fe0609142fa4b71f
Uclécio <uclcio@aphonsiano.edu.br>
2024-02-07 22:07:13 -0300

    Exercicio de Media Concluido

--- media/media.py
+++ media/media.py
@@ -3,9 +3,13 @@
 
 
 valor_primeiro = int(input("informe o primeiro valor:"))
+
 valor_segundo = int(input("informe o segundo valor:"))
+
 valor_terceiro = int(input("informe o terceiro valor:"))
+
 valor_quarto = int(input("informe o quarto valor:"))
+
 soma_dos_valores = valor_primeiro + valor_segundo + valor_terceiro + valor_quarto 
 media = soma_dos_valores / 4
 print(soma_dos_valores)
